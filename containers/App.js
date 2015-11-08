dd9273a9cf0f695c8ed467ce8a7c11938daa1f0d
Phil <birkal@outlook.com>
2015-11-08 12:27:21 -0800

    Update app container

--- containers/App.js
+++ containers/App.js
@@ -1,16 +1,42 @@
-import { bindActionCreators } from 'redux'
+import React, {Component, PropTypes} from 'react';
 import { connect } from 'react-redux'
 import Counter from '../components/Counter'
-import * as CounterActions from '../actions/counter'
+import {addTodo, completeTodo, setVisibilityFilter} from '../actions/todos';
+import {VisibilityFilters} from '../constants/TodoFilters';
+import AddTodo from '../components/AddTodo';
+import TodoList from '../components/TodoList';
+import Footer from '../components/Footer';
 
-function mapStateToProps(state) {
-  return {
-    counter: state.counter
+class App extends Component {
+  render() {
+    const {dispatch, visibleTodos, visibilityFilter} = this.props;
+    return (
+      <div>
+        <AddTodo onAddClick={text => dispatch(addTodo(text))} />
+        <TodoList todos={visibleTodos} onTodoClick={index => dispatch(completeTodo(index))} />
+        <Footer filter={visibilityFilter} onFilterChange={nextFilter => dispatch(setVisibilityFilter(nextFilter))} />
+      </div>
+    );
   }
 }
 
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(CounterActions, dispatch)
+function selectTodos(todos, filter) {
+  switch (filter) {
+    case VisibilityFilters.SHOW_ALL:
+      return todos
+    case VisibilityFilters.SHOW_COMPLETED:
+      return todos.filter(todo => todo.completed)
+    case VisibilityFilters.SHOW_ACTIVE:
+      return todos.filter(todo => !todo.completed)
+  }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Counter)
+function mapStateToProps(state) {
+  return {
+    counter: state.counter,
+    visibleTodos: selectTodos(state.todos, state.visibilityFilter),
+    visibilityFilter: state.visibilityFilter
+  }
+}
+
+export default connect(mapStateToProps)(App);
