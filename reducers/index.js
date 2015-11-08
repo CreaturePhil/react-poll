231e47f47fd8aa33d300cf98246aa3808ed1d6aa
Phil <birkal@outlook.com>
2015-11-08 12:15:57 -0800

    Add reducers

--- reducers/index.js
+++ reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux'
 import counter from './counter'
+import {visibilityFilter, todos} from './todos';
 
 const rootReducer = combineReducers({
-  counter
+  counter,
+  visibilityFilter,
+  todos
 })
 
 export default rootReducer
---
+++ reducers/todos.js
@@ -0,0 +1,28 @@
+import {ADD_TODO, COMPLETE_TODO, SET_VISIBILITY_FILTER} from '../constants/ActionTypes';
+import {VisibilityFilters} from '../constants/TodoFilters';
+
+const {SHOW_ALL} = VisibilityFilters;
+
+export function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
+}
+
+export function todos(state = [], action) {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, {text: action.text, completed: false}];
+    case COMPLETE_TODO:
+      return [
+        ...state.slice(0, action.index),
+        {...state[action.index], completed: true},
+        ...state.slice(action.index + 1)
+      ];
+    default:
+      return state;
+  }
+}
