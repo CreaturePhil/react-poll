9518969b309cc391f568a15232b5862711669864
Phil <birkal@outlook.com>
2015-11-14 21:23:06 -0800

    Remove redux

--- scripts/containers/App.js
+++ scripts/containers/App.js
@@ -1,17 +1,15 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
-import Counter from '../components/Counter';
-import { increment, decrement, incrementIfOdd, incrementAsync } from '../actions/counter';
-import { addTodo, completeTodo, setVisibilityFilter } from '../actions/todos';
-import { VisibilityFilters } from '../constants/TodoFilters';
-import AddTodo from '../components/AddTodo';
-import TodoList from '../components/TodoList';
-import Footer from '../components/Footer';
 import NewPoll from '../components/NewPoll';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+
   render() {
-    const { dispatch, visibleTodos, visibilityFilter, counter } = this.props;
     return (
       <div className='container'>
         <NewPoll />
@@ -20,36 +18,4 @@ class App extends Component {
   }
 }
 
-App.propTypes = {
-  visibleTodos: PropTypes.arrayOf(PropTypes.shape({
-    text: PropTypes.string.isRequired,
-    completed: PropTypes.bool.isRequired
-  })),
-  visibilityFilter: PropTypes.oneOf([
-    'SHOW_ALL',
-    'SHOW_COMPLETED',
-    'SHOW_ACTIVE'
-  ]).isRequired,
-  counter: PropTypes.number.isRequired
-};
-
-function selectTodos(todos, filter) {
-  switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
-      return todos;
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(todo => todo.completed);
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(todo => !todo.completed);
-  }
-}
-
-function mapStateToProps(state) {
-  return {
-    counter: state.counter,
-    visibleTodos: selectTodos(state.todos, state.visibilityFilter),
-    visibilityFilter: state.visibilityFilter
-  };
-}
-
-export default connect(mapStateToProps)(App);
+export default App;
